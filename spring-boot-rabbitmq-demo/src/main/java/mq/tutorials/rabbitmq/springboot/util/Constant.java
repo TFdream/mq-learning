88b661a446d6c1dfdde1ea6e8d76279d33561f5a
Ricky <ricky_feng@163.com>
2018-07-11 00:14:19 +0800

    Add RabbitMQ Spring Boot demo

---
+++ spring-boot-rabbitmq-demo/src/main/java/mq/tutorials/rabbitmq/springboot/util/Constant.java
@@ -0,0 +1,13 @@
+package mq.tutorials.rabbitmq.springboot.util;
+
+/**
+ * @author Ricky Fung
+ */
+public class Constant {
+
+    public static final String EXCHANGE_NAME = "demo-exchange";
+
+    public static final String QUEUE_NAME = "demo";
+
+    public static final String ROUTING_KEY = "test";
+}
---
+++ spring-boot-rabbitmq-demo/src/test/java/mq/tutorials/rabbitmq/AppTest.java
@@ -0,0 +1,16 @@
+package mq.tutorials.rabbitmq;
+
+import org.junit.Test;
+
+/**
+ * @author Ricky Fung
+ * @version 1.0
+ * @since 2018-07-04 22:58
+ */
+public class AppTest {
+
+    @Test
+    public void testApp() {
+
+    }
+}
